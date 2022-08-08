4dc71c3c32d0fa2680d6fb1b276dd61e1f2bb7f9
Derek Cooper <derekcooper20@gmail.com>
2022-08-08 16:50:53 -0600

    Adds code

--- game.js
+++ game.js
@@ -4,14 +4,19 @@ class Game {
     this.winConditions = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6];
     this.redPlayer = new Player('<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/redpill.jpg">');
     this.bluePlayer =  new Player('<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/bluepill.jpeg">');
-    this.currentPlayer = this.redplayer.id;
+    this.currentPlayer = this.redplayer;
   }
 
+  // resetGame() {
+  //   this.currentPlayer = this.redplayer;
+  //   this.board = ['', '', '', '', '', '', '', '', ''];
+  // }
 
   choosePlayer(id) {
-    if (this.board[id] === this.currentPlayer) {
+    this.board[id] = this.choosePlayer.token;
+    if (this.board[id] === this.currentPlayer.id) {
       this.currentPlayer = this.bluePlayer;
-    } else if (this.board[id] === this.bluePlayer) {
+    } else if (this.board[id] === this.bluePlayer.id) {
       this.currentPlayer = this.redPlayer;
     }
   }
