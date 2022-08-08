7868d6a9ce1a8c608f12435f7ef99dfddacbdd20
Derek Cooper <derekcooper20@gmail.com>
2022-08-08 15:31:40 -0600

    Adds player info

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 class Player {
-  constructor(token, id) {
-    this.token = token
+  constructor(token,id) {
+    this.token = token;
     this.id = id;
     this.wins = 0;
     this.winner = false;
