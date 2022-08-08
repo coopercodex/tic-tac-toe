455e0d5fbab08bdebe53a1825c0f89119ed2b220
Derek Cooper <derekcooper20@gmail.com>
2022-08-08 15:30:01 -0600

    Adds properties

---
+++ game.js
@@ -0,0 +1,11 @@
+class Game {
+  constructor() {
+    this.board = ['', '', '', '', '', '', '', '', ''];
+    this.winConditions = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6];
+    this.redplayer = new Player(<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/redpill.jpg"></img>);;
+    this.bluePlayer =  new Player(<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/bluepill.jpeg"></img>);;
+    this.currentPlayer = this.redplayer;
+  }
+   
+
+
