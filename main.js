689e938e1a26b186006dcd1d52d6c0b91966e8e1
Derek Cooper <derekcooper20@gmail.com>
2022-08-09 09:39:17 -0600

    Adds functions to display win

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 // QuerySelectors
 var gameBoard = document.querySelector('.game-board');
 var gameBox = document.querySelectorAll('.game-box');
-
+var ref = document.querySelector('.referee');
+var redPillWins = document.querySelector('.red-pill-wins');
+var bluePillWins = document.querySelector('.blue-pill-wins');
 
 //object instances
 var startGame = new Game();
 
 
 // EventListeners
-for (var i = 0; i < gameBox.length; i++) {
-gameBox[i].addEventListener('click', markSpace);
-}
+gameBoard.addEventListener('click', markSpace);
+
 
 
 // functions
 function markSpace(event) {
-  if (event.target.innerHTML === '') {
-    event.target.innerHTML = startGame.currentPlayer.token;
-    startGame.choosePlayer(event.target.id);
-    console.log(startGame.currentPlayer)
+
+  if (!event.target.innerText) {
+    for (var i = 0; i < gameBox.length; i++) {
+      if (gameBox[i].id === event.target.id) {
+        event.target.innerHTML = startGame.currentPlayer.token;
+        startGame.trackPlayerSpaces(i);
+        startGame.choosePlayer(event.target.id);
+      }
+    }
   }
 }
+
+function resetGame() {
+  for (var i = 0; i < gameBox.length; i++) {
+    gameBox[i].innerHTML = "";
+  }
+  redPillWins.innerHTML = startGame.redPlayer.wins;
+  bluePillWins.innerHTML = startGame.bluePlayer.wins;
+  startGame.resetData();
+}
+
+function declareWinner() {
+  ref.innerHTML = `Winner ${startGame.currentPlayer.token}`
+}
+
