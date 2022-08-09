5d2715f1e7b9b52b9632a8a0b52c61dd3c9dffd5
Derek Cooper <derekcooper20@gmail.com>
2022-08-08 22:53:10 -0600

    Fixed show pill bug

--- main.js
+++ main.js
@@ -3,22 +3,21 @@ var gameBoard = document.querySelector('.game-board');
 var gameBox = document.querySelectorAll('.game-box');
 
 
-
+//object instances
+var startGame = new Game();
 
 
 // EventListeners
 for (var i = 0; i < gameBox.length; i++) {
 gameBox[i].addEventListener('click', markSpace);
 }
-//object instances
-var startGame = new Game();
+
 
 // functions
-function markSpace(e) {
-
-  if (e.target.innerHTML === '') {
-    e.target.innerHTML = startGame.currentPlayer;
-    startGame.choosePlayer(e.target.id);
-    console.log(e.target.id)
+function markSpace(event) {
+  if (event.target.innerHTML === '') {
+    event.target.innerHTML = startGame.currentPlayer.token;
+    startGame.choosePlayer(event.target.id);
+    console.log(startGame.currentPlayer)
   }
 }
