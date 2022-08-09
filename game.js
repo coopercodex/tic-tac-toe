073754453097ccad316cb44e41b8b79412da4a13
Derek Cooper <derekcooper20@gmail.com>
2022-08-08 22:50:36 -0600

    Fixed bugs

--- game.js
+++ game.js
@@ -2,25 +2,25 @@ class Game {
   constructor() {
     this.board = ['', '', '', '', '', '', '', '', ''];
     this.winConditions = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6];
-    this.redPlayer = new Player('<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/redpill.jpg">');
-    this.bluePlayer =  new Player('<img src="/Users/derekcooper/turing_work/1mod/tic-tac-toe/assets/bluepill.jpeg">');
-    this.currentPlayer = this.redplayer;
+    this.redPlayer = new Player(`<img src="assets/redpill.jpeg">`,1);
+    this.bluePlayer =  new Player(`<img src="assets/bluepill.jpeg">`,2);
+    this.currentPlayer = this.redPlayer;
   }
 
-  // resetGame() {
-  //   this.currentPlayer = this.redplayer;
-  //   this.board = ['', '', '', '', '', '', '', '', ''];
-  // }
+  resetGame() {
+    this.currentPlayer = this.redplayer;
+    this.board = ['', '', '', '', '', '', '', '', ''];
+  }
 
   choosePlayer(id) {
-    this.board[id] = this.choosePlayer.token;
-    if (this.board[id] === this.currentPlayer.id) {
+    this.board[id] = this.currentPlayer.token;
+    if (this.currentPlayer === this.redPlayer) {
       this.currentPlayer = this.bluePlayer;
-    } else if (this.board[id] === this.bluePlayer.id) {
+    } else if (this.currentPlayer != this.redPlayer) {
       this.currentPlayer = this.redPlayer;
     }
   }
-}
+  }
 
 
 
